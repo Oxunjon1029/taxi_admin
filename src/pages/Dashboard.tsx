c0c0b832dd5cdfdfaebb0d6bc5491f885a565cf0
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-04-21 17:29:47 +0500

    map again changed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -52,7 +52,7 @@ const Dashboard = () => {
           <div>
             <Map
               width='98.7%'
-              height='100%'
+              height={400}
               defaultState={{ center: [41.311151, 69.279737], zoom: 6 }}>
               <Placemark
                 geometry={[41.311151, 69.279737]}
